cfc4e038107629d37d9395a0773ef35dcf8dea8d
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-02-23 22:31:49 +0800

    feat: login activity added user validation and error handling
    The commit introduces `BuzyUser` class to store registered users and their credentials.
    It enhances the login functionality by:
    - checking if a user is registered before allowing login,
    - prompting to register if no user is found,
    - implementing credential validation to check user name and password,
    - and displaying error messages for incorrect credentials or empty fields.

--- app/src/main/java/io/buzypc/app/ui/LoginActivity.kt
+++ app/src/main/java/io/buzypc/app/ui/LoginActivity.kt
@@ -8,11 +8,13 @@ import android.widget.EditText
 import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import io.buzypc.app.R
+import io.buzypc.app.data.BuzyUser
 
 
 class LoginActivity : AppCompatActivity() {
@@ -26,23 +28,38 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+        val userDetails = BuzyUser(this)
+
         val edittext_username = findViewById<EditText>(R.id.username)
         val edittext_password = findViewById<EditText>(R.id.password)
 
         val button_login = findViewById<Button>(R.id.loginButton)
         val button_register = findViewById<Button>(R.id.registerButton)
 
-        button_login.setOnClickListener(){
+        button_login.setOnClickListener {
+            if(!userDetails.isUserRegistered()) {
+                AlertDialog.Builder(this)
+                    .setTitle("Profile Not Found")
+                    .setMessage("Uh-oh! It seems we didn't find an existing account. Create an account?")
+                    .setPositiveButton("Register") { _,_ ->
+                        val intent = Intent(this, RegisterActivity::class.java)
+                        startActivity(intent)
+                    }
+                    .setNegativeButton("Cancel", null)
+                    .show()
+                return@setOnClickListener
+            }
+
             if(edittext_username.text.isNullOrEmpty() || edittext_password.text.isNullOrEmpty()){
                 Toast.makeText(this,"Username and Password must not be empty",Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
 
-            if(edittext_username.text.contentEquals("csit", true) && edittext_password.text.contentEquals("1234",false)){
+            if(userDetails.validateLogin(edittext_username.text.toString(), edittext_password.text.toString())) {
                 val intent = Intent(this, LandingPageActivity::class.java)
                 startActivity(intent)
             }
-            else{
+            else {
                 Toast.makeText(this, "Invalid credentials", Toast.LENGTH_LONG).show()
             }
         }
