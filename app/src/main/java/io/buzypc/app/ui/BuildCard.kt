54fd05b919d494c791d25c1e6d7a0241d3e44037
Austine <austinelomocso@gmail.com>
2025-02-23 23:46:00 +0800

    feat: existing builds screen

--- app/src/main/java/io/buzypc/app/ui/BuildCard.kt
+++ app/src/main/java/io/buzypc/app/ui/BuildCard.kt
@@ -1,36 +1,21 @@
 package io.buzypc.app.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import io.buzypc.app.R
 
-class LandingPageActivity : AppCompatActivity() {
+class BuildCard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_landing_page)
+        setContentView(R.layout.activity_build_card)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val add_button = findViewById<ImageView>(R.id.add_build)
-        add_button.setOnClickListener(){
-            val intent = Intent(this,NewBuildActivity::class.java)
-            startActivity(intent)
-        }
-
-        val btnCartButton = findViewById<ImageView>(R.id.view_cart)
-        btnCartButton.setOnClickListener(){
-            val intent = Intent(this, BuildsListActivity::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/io/buzypc/app/ui/NewBuildActivity.kt
+++ app/src/main/java/io/buzypc/app/ui/NewBuildActivity.kt
@@ -1,6 +1,8 @@
 package io.buzypc.app.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +19,12 @@ class NewBuildActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val profileViewButton = findViewById<ImageView>(R.id.image_profileButton)
+
+        profileViewButton.setOnClickListener(){
+            val intent = Intent(this, ProfileViewActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
