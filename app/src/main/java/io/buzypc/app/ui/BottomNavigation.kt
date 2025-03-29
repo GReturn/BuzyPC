0cbfb1b3786d4c8b582ae185406bbeea47082200
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-03-29 13:29:37 +0800

    rwrite nav

--- app/src/main/java/io/buzypc/app/ui/BottomNavigation.kt
+++ app/src/main/java/io/buzypc/app/ui/BottomNavigation.kt
@@ -3,44 +3,52 @@ package io.buzypc.app.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.buzypc.app.R
-import io.buzypc.app.databinding.ActivityBottomNavigationBinding
-import io.buzypc.app.ui.fragments.TEMPORARY_AboutDevelopersFragment
-import io.buzypc.app.ui.fragments.BuildListFragment
-import io.buzypc.app.ui.fragments.LandingPageFragment
-import io.buzypc.app.ui.fragments.NewBuildFragment
-import io.buzypc.app.ui.fragments.SettingsFragment
 
 class BottomNavigation : AppCompatActivity() {
-    private var binding: ActivityBottomNavigationBinding? = null
+//    private var binding: ActivityBottomNavigationBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
-        setContentView(binding!!.getRoot())
 
-        // Restore or set default fragment
-        if (savedInstanceState == null) {
-            replaceFragment(LandingPageFragment(), false, null)
-        }
+        setContentView(R.layout.activity_navigation)
 
-        // Bottom navigation items are considered top-level destinations
-        binding!!.bottomNavigationView.setOnItemSelectedListener { item ->
-            val itemId = item.itemId
-            if (itemId == R.id.landing_page) {
-                replaceFragment(LandingPageFragment(), false, null)
-            } else if (itemId == R.id.about_devs) {
-                replaceFragment(BuildListFragment(), false, null)
-            } else if (itemId == R.id.build_list) {
-                replaceFragment(TEMPORARY_AboutDevelopersFragment(), false, null)
-            } else if (itemId == R.id.add_build) {
-                replaceFragment(NewBuildFragment(), false, null)
-            } else if (itemId == R.id.profile_view) {
-                val args = Bundle()
-                args.putBoolean("refresh", true) // Signal to refresh data
-                replaceFragment(SettingsFragment(), false, args)
-            }
-            true
-        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.navController)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+//        bottomNavigationView.setupWithNavController(navController)
+
+//        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+//        setContentView(binding!!.getRoot())
+//
+//        // Restore or set default fragment
+//        if (savedInstanceState == null) {
+//            replaceFragment(LandingPageFragment(), false, null)
+//        }
+//
+//        // Bottom navigation items are considered top-level destinations
+//        binding!!.bottomNavigationView.setOnItemSelectedListener { item ->
+//            val itemId = item.itemId
+//            if (itemId == R.id.landing_page) {
+//                replaceFragment(LandingPageFragment(), false, null)
+//            } else if (itemId == R.id.about_devs) {
+//                replaceFragment(BuildListFragment(), false, null)
+//            } else if (itemId == R.id.build_list) {
+//                replaceFragment(TEMPORARY_AboutDevelopersFragment(), false, null)
+//            } else if (itemId == R.id.add_build) {
+//                replaceFragment(NewBuildFragment(), false, null)
+//            } else if (itemId == R.id.profile_view) {
+//                val args = Bundle()
+//                args.putBoolean("refresh", true) // Signal to refresh data
+//                replaceFragment(SettingsFragment(), false, args)
+//            }
+//            true
+//        }
     }
 
     private fun replaceFragment(fragment: Fragment, addToBackStack: Boolean, args: Bundle?) {
