708d2ec42520c55da7cd47894c3f95ac8752f7ac
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-04-15 09:21:40 +0800

    feat: added text limit for build name (10 chars)

--- app/src/main/java/io/buzypc/app/ui/navigation/fragments/NewBuildFragment.kt
+++ app/src/main/java/io/buzypc/app/ui/navigation/fragments/NewBuildFragment.kt
@@ -2,6 +2,8 @@ package io.buzypc.app.ui.navigation.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,12 +17,28 @@ import io.buzypc.app.ui.BuildSummaryActivity
 
 class NewBuildFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_new_build, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val etBudgetInput = view.findViewById<EditText>(R.id.et_budgetInput)
         val etBuildName = view.findViewById<EditText>(R.id.et_buildName)
 
         val btnBuild = view.findViewById<Button>(R.id.btn_build)
+
+        etBuildName.addTextChangedListener( object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(s?.length == 10) Toast.makeText(this@NewBuildFragment.context, "Only 10 characters allowed for the build name.", Toast.LENGTH_LONG).show()
+            }
+        })
 
         btnBuild.setOnClickListener {
             if(etBuildName.text.isNullOrEmpty()) etBuildName.setText("New Build")
@@ -39,9 +57,5 @@ class NewBuildFragment : Fragment() {
             val intent = Intent(requireActivity(), BuildSummaryActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_new_build, container, false)
     }
 }
