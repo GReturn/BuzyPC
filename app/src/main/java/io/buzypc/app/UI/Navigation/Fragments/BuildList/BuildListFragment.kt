c5aa6b8785937cf6c4d28d4b2f7726e03a06da51
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-04-29 14:37:10 +0800

    build-list-fragment, radar-chart-view-fragment: removed unused imports

--- app/src/main/java/io/buzypc/app/UI/Navigation/Fragments/BuildList/BuildListFragment.kt
+++ app/src/main/java/io/buzypc/app/UI/Navigation/Fragments/BuildList/BuildListFragment.kt
@@ -9,7 +9,6 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.view.doOnLayout
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.buzypc.app.Data.AppSession.BuzyUserAppSession
 import io.buzypc.app.Data.BuildData.PCBuild
--- app/src/main/java/io/buzypc/app/UI/Widget/RadarChartViewFragment.kt
+++ app/src/main/java/io/buzypc/app/UI/Widget/RadarChartViewFragment.kt
@@ -9,7 +9,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
