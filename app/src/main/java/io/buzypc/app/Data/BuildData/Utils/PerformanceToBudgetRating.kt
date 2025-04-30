bb8db72cc7c94c99d81fa627181807137e889370
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-04-30 10:56:36 +0800

    feat: removed PerformanceToBudgetRating enum class

--- app/src/main/java/io/buzypc/app/Data/BuildData/Utils/PerformanceToBudgetRating.kt
+++
@@ -1,8 +0,0 @@
-package io.buzypc.app.Data.BuildData.Utils
-
-enum class PerformanceToBudgetRating {
-    UNDER_BUDGET_LOW_PERFORMANCE,
-    WELL_BALANCED,
-    PERFORMANCE_FOCUSED,
-    OVERSPENT_LOW_RETURN
-}
