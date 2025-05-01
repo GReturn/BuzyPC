95c98383d83cdc6ba465b08d2a7d257f23547286
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-05-01 10:44:23 +0800

    feat: modified Store data class, removed unnecessary fields

--- app/src/main/java/io/buzypc/app/Data/BuildData/Store.kt
+++ app/src/main/java/io/buzypc/app/Data/BuildData/Store.kt
@@ -2,8 +2,6 @@ package io.buzypc.app.Data.BuildData
 
 data class Store(
     val name: String,
-    val imageUrl: String,
     val vendorSite: String,
-    val associatedItem: String, // Name of the PC component
     val priceRange: ClosedFloatingPointRange<Double>
 )
